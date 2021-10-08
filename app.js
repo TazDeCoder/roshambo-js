c86b2d059da37ecd6f3aa80ffa2b9b73b950b8a5
tarwat-uddin <t.uddin9121@gmail.com>
2021-10-08 15:22:22 +0100

    Refractor app and fix bugs

--- app.js
+++ app.js
@@ -2,20 +2,22 @@
 
 // Selecting elements
 // Buttons
-const [rockBtn, paperBtn, scissorsBtn] = document
+const handBtns = document
   .querySelector("#btn--hands")
   .getElementsByClassName("btn");
+const [rockBtn, paperBtn, scissorsBtn] = handBtns;
 const throwBtn = document.querySelector("#btn--throw");
 const resetBtn = document.querySelector("#btn--reset");
 const closeBtn = document.querySelector("#btn--close");
-// General elements
-const gameEl = document.querySelector("#game--label");
-const name0El = document.querySelector("#name--0");
-const score0El = document.querySelector("#score--0");
-const score1El = document.querySelector("#score--1");
-const hand0El = document.querySelector("#hand--0");
-const hand1El = document.querySelector("#hand--1");
-// Effects
+// Labels
+const gameLbl = document.querySelector("#game--label");
+const name0Lbl = document.querySelector("#name--0");
+const score0Lbl = document.querySelector("#score--0");
+const score1Lbl = document.querySelector("#score--1");
+// Images
+const hand0Img = document.querySelector("#hand--0");
+const hand1Img = document.querySelector("#hand--1");
+// Misc.
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 
@@ -42,33 +44,33 @@ function init() {
   game.flag = true;
   currentHand = rockBtn;
   // Clean-up GUI
-  currentHand.classList.add("btn--active");
+  rockBtn.classList.add("btn--active");
   paperBtn.classList.remove("btn--active");
   scissorsBtn.classList.remove("btn--active");
-  hand1El.classList.add("invisible");
-  gameEl.classList.remove("invisible");
-  gameEl.textContent = "Select an Element!";
-  gameEl.style.color = "#000;";
-  score0El.textContent = game.scores.player;
-  score1El.textContent = game.scores.computer;
+  hand1Img.classList.add("invisible");
+  gameLbl.textContent = "Select an Element!";
+  gameLbl.style.color = "#000";
+  score0Lbl.textContent = game.scores.player;
+  score1Lbl.textContent = game.scores.computer;
   throwBtn.textContent = `Throw 👋 ${currentHand.textContent}!`;
 }
 
 function loadGame() {
-  // Needs to only run once when the app is open
+  // Loads first time app open
   playerName = prompt("Ready!? Enter Player Name: ");
   if (!playerName) return alert("MUST SPEICFY NAME!");
-  name0El.textContent = playerName;
+  name0Lbl.textContent = playerName;
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 }
 
-function handButtonPressed(self) {
+function updateGame(self) {
   game.playerHand1 = self.value;
   self.classList.toggle("btn--active");
   currentHand.classList.toggle("btn--active");
   currentHand = self;
-  replaceHands(game.playerHand1);
+  hand0Img.src = `./assets/images/${game.playerHand1}-right.png`;
+  hand1Img.classList.add("invisible");
   throwBtn.textContent = `Throw 👋 ${currentHand.textContent}!`;
 }
 
@@ -79,106 +81,67 @@ function generateHand() {
   return hands[index];
 }
 
-function replaceHands(hand) {
-  // Replaces current hand with one (argument) that is passed
-  hand0El.src = `./assets/images/${hand}-right.png`;
-  hand1El.classList.add("invisible");
-}
-
 function displayHands() {
   game.playerHand2 = generateHand();
-  hand1El.src = `./assets/images/${game.playerHand2}-left.png`;
-  hand1El.classList.remove("invisible");
+  hand1Img.src = `./assets/images/${game.playerHand2}-left.png`;
+  hand1Img.classList.remove("invisible");
 }
 
-function workoutRoundWinner() {
+function isGameWinner() {
+  // Checks if either player or CPU wins the game
+  if (game.scores.player === game.rounds) {
+    gameLbl.textContent = `${playerName} Wins 🏆!`;
+    gameLbl.style.color = "#f00";
+    game.flag = false;
+  } else if (game.scores.computer === game.rounds) {
+    gameLbl.textContent = "CPU Wins 🤖!";
+    gameLbl.style.color = "#00f";
+    game.flag = false;
+  }
+  return false;
+}
+
+function calcRoundWinner() {
   // Checks if player 1 hand matches player 2
   if (game.playerHand1 === game.playerHand2) {
-    gameEl.textContent = "This Round is a Draw!";
+    gameLbl.textContent = "This Round is a Draw!";
     // Checks if player 1 beats player 2
   } else if (
     (game.playerHand1 === "rock" && game.playerHand2 === "scissors") ||
     (game.playerHand1 === "scissors" && game.playerHand2 === "paper") ||
     (game.playerHand1 === "paper" && game.playerHand2 === "rock")
   ) {
-    gameEl.textContent = `${playerName} Wins This Round!`;
-    game.scores.player += 1;
-    score0El.textContent = game.scores.player;
+    gameLbl.textContent = `${playerName} Wins This Round!`;
+    game.scores.player++;
+    score0Lbl.textContent = game.scores.player;
 
     // Or otherwise, player 2 beats player 1
   } else {
-    gameEl.textContent = "CPU Wins This Round!";
-    game.scores.computer += 1;
-    score1El.textContent = game.scores.computer;
-  }
-}
-
-function workoutMatchWinner() {
-  if (game.scores.player === game.rounds) {
-    gameEl.textContent = `${playerName} Wins 🏆!`;
-    game.flag = false;
-  } else if (game.scores.computer === game.rounds) {
-    gameEl.textContent = "CPU Wins 🤖!";
-    game.flag = false;
+    gameLbl.textContent = "CPU Wins This Round!";
+    game.scores.computer++;
+    score1Lbl.textContent = game.scores.computer;
   }
 }
 
 // Button functionalities
-// Handles hand button events
-rockBtn.addEventListener("click", function () {
-  if (game.flag) handButtonPressed(this);
-});
-rockBtn.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    if (game.flag) {
+for (const btn of handBtns) {
+  btn.addEventListener("click", function () {
+    if (game.flag) updateGame(this);
+  });
+  btn.addEventListener("keyup", function (e) {
+    if (game.flag && e.key === "Enter" && !isGameWinner()) {
       displayHands();
-      workoutRoundWinner();
-      workoutMatchWinner();
+      calcRoundWinner();
     }
-  }
-});
-paperBtn.addEventListener("click", function () {
-  if (game.flag) handButtonPressed(this);
-});
-paperBtn.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    if (game.flag) {
-      displayHands();
-      workoutRoundWinner();
-      workoutMatchWinner();
-    }
-  }
-});
-scissorsBtn.addEventListener("click", function () {
-  if (game.flag) handButtonPressed(this);
-});
-scissorsBtn.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    if (game.flag) {
-      displayHands();
-      workoutRoundWinner();
-      workoutMatchWinner();
-    }
-  }
-});
+  });
+}
 
 throwBtn.addEventListener("click", function () {
-  if (game.flag) {
+  if (game.flag && !isGameWinner()) {
     displayHands();
-    workoutRoundWinner();
-    workoutMatchWinner();
+    calcRoundWinner();
   }
 });
-
-// document.addEventListener("keydown", function (e) {
-//   if (e.key === "Enter") {
-//     if (game.flag) {
-//       displayHands();
-//       workoutRoundWinner();
-//       workoutMatchWinner();
-//     }
-//   }
-// });
 
 resetBtn.addEventListener("click", init);
 closeBtn.addEventListener("click", loadGame);
