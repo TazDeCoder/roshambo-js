4b3cc207f91055ab24b7dfabfda5291d4d1f8a7a
tarwat-uddin <t.uddin9121@gmail.com>
2021-10-30 16:39:40 +0100

    Refractor app.js
    Minor improvements

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 "use scrict";
 
-// Selecting HTML elements
+////////////////////////////////////////////////
+////// Selecting HTML elements
+///////////////////////////////////////////////
+
 // Buttons
 const btnsHand = document.querySelectorAll(".selection--hands .selection__btn");
 const btnRock = document.querySelector(".selection__btn--rock");
@@ -22,21 +25,19 @@ const inputRound = document.querySelector(".modal__input--round");
 // Misc.
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-// --- OTHERS ---
+// Others
 const selectionModes = document.querySelector(".selection--modes");
 const selectionHands = document.querySelector(".selection--hands");
 
-// Initial variables
-let playerName,
-  currHand,
-  lastPlayerHand,
-  playerHand,
-  computerHand,
-  roundWinner,
-  flag;
+////////////////////////////////////////////////
+////// Global variables
+///////////////////////////////////////////////
+
+let playerName, currPlayerHand, lastPlayerHand, flag;
 
 const game = {
   mode: "",
+  roundWinner: "",
   rounds: 0,
   scores: {
     player: 0,
@@ -45,43 +46,46 @@ const game = {
   winConditions: {
     player: [
       {
-        hand1: "rock",
-        hand2: "scissors",
+        hand1: btnRock,
+        hand2: btnScissors,
       },
       {
-        hand1: "paper",
-        hand2: "rock",
+        hand1: btnPaper,
+        hand2: btnRock,
       },
       {
-        hand1: "scissors",
-        hand2: "paper",
+        hand1: btnScissors,
+        hand2: btnPaper,
       },
     ],
     computer: [
       {
-        hand1: "rock",
-        hand2: "paper",
+        hand1: btnRock,
+        hand2: btnPaper,
       },
       {
-        hand1: "paper",
-        hand2: "scissors",
+        hand1: btnPaper,
+        hand2: btnScissors,
       },
       {
-        hand1: "scissors",
-        hand2: "rock",
+        hand1: btnScissors,
+        hand2: btnRock,
       },
     ],
   },
 };
 
+////////////////////////////////////////////////
+////// Game UI Setup
+///////////////////////////////////////////////
+
 function init() {
   // Reset game values
-  currHand = btnRock;
-  lastPlayerHand = roundWinner = "";
-  playerHand = currHand.value;
-  flag = true;
+  currPlayerHand = btnRock;
+  lastPlayerHand = game.roundWinner = "";
   game.scores.player = game.scores.computer = 0;
-  // Clean-up GUI
+  flag = true;
+  // Clean-up UI
   btnRock.classList.add("selection__btn--active");
   btnPaper.classList.remove("selection__btn--active");
   btnScissors.classList.remove("selection__btn--active");
@@ -103,84 +107,85 @@ function loadGame() {
 }
 
 function updateGame() {
-  calcHand();
-  throwHands();
+  throwHands(currPlayerHand, calcHand());
   checkGameWinner();
 }
 
-// Game UI Functions
 function updateUI() {
-  imageHand0.src = `./assets/images/hands/${playerHand}-right.png`;
-  btnThrow.textContent = `Throw 👋 ${currHand.textContent
+  imageHand0.src = `./assets/images/hands/${currPlayerHand.value}-right.png`;
+  btnThrow.textContent = `Throw 👋 ${currPlayerHand.textContent
     .slice(0, 15)
     .concat("(Enter)")}!`;
 }
 
-// Game Logic Functions
+////////////////////////////////////////////////
+////// Game Logic
+///////////////////////////////////////////////
+
 function calcHand() {
   // NOTE: PLAN TO ADD DETAILED COMMENTS --> TO MAKE CODE MORE CLEAR + READABLE
-  const generateRandomHand = (hands = ["rock", "paper", "scissors"]) => {
+  const generateRandomHand = (hands = [btnRock, btnPaper, btnScissors]) => {
     const idx = Math.floor(Math.random() * hands.length);
     return hands[idx];
   };
-  let hand, idx;
+  let computerHand, hand, idx;
   const { player: playerWinConds, computer: computerWinConds } =
     game.winConditions;
   // --- EASY MODE ---
   if (game.mode === "easy") {
-    // Loops over object to workout idx (see below)
     playerWinConds.map((hands, i) => {
       if (lastPlayerHand?.value === hands.hand1) idx = i;
     });
     hand =
-      roundWinner === "player"
-        ? lastPlayerHand.value
+      game.roundWinner === "player"
+        ? lastPlayerHand
         : playerWinConds[idx]?.hand2;
   } // --- HARD MODE ---
   else if (game.mode === "hard") {
     const arr = new Array(5);
-    // Loops over object to workout idx (see below)
     computerWinConds.map((hands, i) => {
       if (lastPlayerHand?.value === hands.hand1) idx = i;
     });
-    if (roundWinner === "player") {
+    if (game.roundWinner === "player") {
       // Filling array --> used for probability
-      arr.fill(lastPlayerHand.value, 0, 2);
+      arr.fill(lastPlayerHand, 0, 2);
       arr.fill(game.winConditions.computer[idx].hand2, 2);
     }
     hand = generateRandomHand(arr);
   }
   computerHand = !hand ? generateRandomHand() : hand;
-  imageHand1.src = `./assets/images/hands/${computerHand}-left.png`;
+  imageHand1.src = `./assets/images/hands/${computerHand.value}-left.png`;
+  return computerHand;
 }
 
-function throwHands() {
-  lastPlayerHand = currHand;
-  const gameHands = [playerHand, computerHand];
-  // Checks if player 1 hand matches player 2
-  if (playerHand === computerHand) {
+function throwHands(h1, h2) {
+  const hands = [h1, h2];
+  lastPlayerHand = h1;
+  // Checks if player hand matches computer hand
+  if (h1 === h2) {
     labelGame.textContent = "This Round is a Draw!";
-    roundWinner = "";
+    game.roundWinner = "";
   } else {
+    // Else either player or computer has won
     const { player: playerWinConds, computer: computerWinConds } =
       game.winConditions;
     for (let i = 0; i < playerWinConds.length; i++) {
       const playerWinCond = Object.values(playerWinConds[i]);
       const computerWinCond = Object.values(computerWinConds[i]);
       // Compares every hand to see if player won this round
-      if (gameHands.every((hand, i) => hand === playerWinCond[i])) {
-        roundWinner = "player";
+      if (hands.every((h, i) => h === playerWinCond[i])) {
+        game.roundWinner = "player";
         game.scores.player++;
         labelScore0.textContent = game.scores.player;
         // Or otherwise, compares every hand again to see if computer won this round
-      } else if (gameHands.every((hand, i) => hand === computerWinCond[i])) {
-        roundWinner = "computer";
+      } else if (hands.every((h, i) => h === computerWinCond[i])) {
+        game.roundWinner = "computer";
         game.scores.computer++;
         labelScore1.textContent = game.scores.computer;
       }
     }
     labelGame.textContent =
-      roundWinner === "player"
+      game.roundWinner === "player"
         ? `${playerName} Wins This Round!`
         : "CPU Wins This Round!";
   }
@@ -193,12 +198,15 @@ function checkGameWinner() {
     game.scores.computer === game.rounds
   ) {
     labelGame.textContent =
-      roundWinner === "player" ? `${playerName} Wins 🏆!` : "CPU Wins 🤖!";
+      game.roundWinner === "player" ? `${playerName} Wins 🏆!` : "CPU Wins 🤖!";
     flag = false;
   }
 }
 
-// Event Handlers
+////////////////////////////////////////////////
+////// Event Handlers
+///////////////////////////////////////////////
+
 selectionModes.addEventListener("click", function (e) {
   const clicked = e.target;
   if (clicked.classList.contains("selection__btn")) {
@@ -230,9 +238,8 @@ function changePlayerHand(self) {
   const [...btns] = selectionHands.querySelectorAll(".selection__btn");
   btns.forEach((btn) => btn.classList.remove("selection__btn--active"));
   self.classList.add("selection__btn--active");
-  currHand.blur();
-  playerHand = self.value;
-  currHand = self;
+  currPlayerHand.blur();
+  currPlayerHand = self;
   updateUI();
 }
 
@@ -240,7 +247,6 @@ document.addEventListener("keyup", function (e) {
   if (flag) {
     switch (e.key.toLowerCase()) {
       case "enter":
-        currHand.blur();
         return updateGame();
       case "q":
         return changePlayerHand(btnRock);
